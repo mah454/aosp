367a2f6d72577357eca38c4caca43c917da2aa01
mah454 <mah454@gmail.com>
2023-09-20 13:31:41 +0330

    Improvement

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,7 @@ set_stuff_for_environment
 lunch
 
 if [[ ! -z $COMPILE_JOB_COUNT ]] && [[ $COMPILE_JOB_COUNT -gt $(nproc) ]] ; then
-    make -j$(nproc) 
     make -j$(nproc) dist
 else 
-    make -j $AOSP_JOB_COUNT 
     make -j $AOSP_JOB_COUNT  dist
 fi
